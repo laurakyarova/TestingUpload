76f5bd1105d6f70ab652b0f0415b4356742c6a93
laurakyarova <laurakyarova@gmail.com>
2023-08-02 16:19:41 -0400

    code to calculate sum of two integers

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int number1=20;
+        int number2=15;
+        System.out.println(number1+number2);
+
+        }
+}
